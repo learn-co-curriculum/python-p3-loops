6f651bc43017a8a417f23f5b59f4cbd32e6d4b86
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-25 10:21:43 -0500

    add solution

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here
-    pass
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
