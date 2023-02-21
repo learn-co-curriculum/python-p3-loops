493d3cfe19bd99fa0e0b6c8c0e503eb87e8a1ef9
Ben Botsford <ben.botsford@flatironschool.com>
2023-02-21 14:07:55 -0600

    fixes #3

--- lib/looping.py
+++ lib/looping.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    for i in range(10, 0, -1):
-        print(i)
-    
-    print("Happy New Year!")
+    # code goes here!
+    pass
 
 def square_integers(int_list):
-    return [i ** 2 for i in int_list]
+    # code goes here!
+    pass
 
 def fizzbuzz():
-    for i in range(1, 101):
-        if not i % 15:
-            print("FizzBuzz")
-        elif not i % 5:
-            print("Buzz")
-        elif not i % 3:
-            print("Fizz")
-        else:
-            print(i)
+    # code goes here!
+    pass
